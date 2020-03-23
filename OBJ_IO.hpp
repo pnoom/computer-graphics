bb0a2ed8b3ccb1780a3cca57ed9987f50aa8c46e
Andrew Page <andrew.page.2017@my.bristol.ac.uk>
2020-03-23 17:24:08 +0000

    I'm totally overcomplicating it. Just use sscanf with different format strings if it fails, or first split the faceTerm and count the tokens.

--- OBJ_IO.hpp
+++ OBJ_IO.hpp
@@ -60,20 +60,42 @@ class OBJ_IO {
       vec3_int vindices;
       vec3_int tindices;
       vec3_int nindices;
+      bool vts, vns;
+      faceData face;
       int i = 0;
       // Repeatedly get "v1/vt1" or similar, then parse /-delimited ints
       while (lineStream >> faceTerm) {
         faceTermStream = istringstream(faceTerm);
-          // Get string of first subTerm
-          getline(faceTermStream, subTerm, '/');
-          vindices[i] = stoi(subTerm, NULL);
-          // Get string of second subTerm. May fail if faceTerm like "v1/"
-          getline(faceTermStream, subTerm, '/');
-          // if (subTerm.size() == 0)
-          tindices[i] = stoi(subTerm, NULL);
-          // Get third subTerm (everything up till end of line)
+        // Get string of first subTerm
+        getline(faceTermStream, subTerm, '/');
+        vindices[i] = stoi(subTerm, NULL);
+
+        // Get string of second subTerm. May fail if faceTerm like "v1/",
+        // or have length zero if like "v1//vn1"
+        getline(faceTermStream, subTerm, '/');
+        // TODO: check that these conditionals behave as intended
+        if (faceTermStream.fail()) {
+          vts = false;
+          vns = false;
+          continue;
+        }
+        else if (subTerm.size() == 0) {
+          vts = false;
+          // Get third subTerm (everything up till newline). May fail if
+          // faceTerm like "v1/" or "v1/vt1"
           getline(faceTermStream, subTerm);
           nindices[i] = stoi(subTerm, NULL);
+          continue;
+        }
+        else {
+          vts = true;
+          tindices[i] = stoi(subTerm, NULL);
+        }
+        
+        // Get third subTerm (everything up till end of line)
+        getline(faceTermStream, subTerm);
+        if (faceTermStream.fail() || )
+        nindices[i] = stoi(subTerm, NULL);
       }
 
         //d = stoi(tokens[0], NULL);
@@ -82,8 +104,7 @@ class OBJ_IO {
         //tindices[0] = e;
         //lineStream >> vindices[0] >> vindices[1] >> vindices[2];
       //face = make_tuple(vindices, tindices); // this should copy these arrays from the temp variables
-
-
+      return face;
     }
 
     // Don't bother having an MTL_Structure class, just use a tuple
