6642d1f5dd4430c0fd3ac10fc104df97bc0384f8
Andrew Page <andrew.page.2017@my.bristol.ac.uk>
2020-04-14 15:46:33 +0100

    Added teapot, increased light intensity, getting weirdness

--- Light.hpp
+++ Light.hpp
@@ -3,10 +3,10 @@
 class Light
 {
   public:
-    glm::vec3 Position = glm::vec3(250.0f, 470.0f, 120.0f);
+    glm::vec3 Position = glm::vec3(500.0f, 500.0f, 500.0f);
     //vec3 lightPos(250.0f, 400.0f, 200.0f);
-    float Intensity = 2000.0f;
-    float Spread = 4.0f;
+    float Intensity = 4000.0f;
+    float Spread = 8.0f;
 
     Light () {}
 
--- RedNoise.cpp
+++ RedNoise.cpp
@@ -143,6 +143,7 @@ vec3 averageVerticesOfFaces(vector<ModelTriangle> faces) {
 void readOBJs() {
   vector<GObject> scene;
   vector<GObject> logo;
+  vector<GObject> teapot;
 
   // This tuple/optional nonsense is just to avoid changing OBJ_IO/Structure too much.
   // For each obj file we load, it may have a texture file. We should have a global
@@ -155,11 +156,17 @@ void readOBJs() {
   //scene = obj_io.scale_multiplicative(WIDTH, scene);
   if (maybeTexture) textures.push_back(maybeTexture.value());
 
-  tie(logo, maybeTexture) = obj_io.loadOBJ("logo.obj");
-  logo = obj_io.scale_additive(logo);
+  //tie(logo, maybeTexture) = obj_io.loadOBJ("logo.obj");
+  //logo = obj_io.scale_additive(logo);
+  //if (maybeTexture) textures.push_back(maybeTexture.value());
+
+  tie(teapot, maybeTexture) = obj_io.loadOBJ("teapot200.obj");
+  teapot = obj_io.scale_additive(teapot);
+  teapot = obj_io.scale_multiplicative(WIDTH, teapot);
   if (maybeTexture) textures.push_back(maybeTexture.value());
 
-  gobjects = joinGObjectVectors(scene, logo);
+  //gobjects = joinGObjectVectors(scene, logo);
+  gobjects = joinGObjectVectors(scene, teapot);
 
   // Find the light gobject, average its vertices, and use that as the light pos
   // (but shift it down slightly first, so it doesn't lie exactly within the
