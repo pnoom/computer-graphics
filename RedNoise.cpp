8bb616d3b98da9617614839fe86e553980eb6c4f
Andrew Page <andrew.page.2017@my.bristol.ac.uk>
2020-04-15 16:28:28 +0100

    Commented out writePPM and delay for now. Flushed stdout so frame numbers get printed during anim

--- RedNoise.cpp
+++ RedNoise.cpp
@@ -840,10 +840,11 @@ void handleEvent(SDL_Event event) {
       if (animating) {
         frame_no ++;
         std::cout << "fr_" << frame_no << "; ";
+        fflush(stdout);
         draw();
-        writePPM();
+        //writePPM();
         // Wait for the PPM to be written. Increase this value for raster/raytrace
-        SDL_Delay(1000);
+        //SDL_Delay(1000);
         rotateTeaPot(10.0f);
         camera.moveAlongAnimArc(-10.0f);
         camera.lookAt(getCentreOf("logo"));
